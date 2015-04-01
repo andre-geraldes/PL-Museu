f71f39e41c5b53b5f69b8cd884fa00a8fea484f1
7patricia <bpatrcia@gmail.com>
2015-04-01 15:06:18 +0100

    Carateres especiais

--- html.c
+++ html.c
@@ -3,6 +3,8 @@
 void newHeader(char *tit, FILE *fp) {
 	fputs("<!DOCTYPE html>",fp);
 	fputs("<html>",fp);
+	fputs("<meta charset=utf-8>",fp);
+	fputs("<link rel=stylesheet type=text/css href=mystyle.css>",fp);
 	//fprintf(fp,"<body bgcolor=#CCFF99>");
 	fprintf(fp, "<title>%s</title>", tit);
 	fputs("<font color=#006699><font face=arial><font size=5><h1><center>Museu da Pessoa</center></h1></font></font></font>",fp);
@@ -15,8 +17,8 @@ void endHtml(FILE * fp) {
 
 void addImg(char * nomeImg, char * descricao, FILE *fp){
 	fputs("<body>",fp);
-	fprintf(fp, "<center><font face=arial><font color=#006600><h2>%s</h2></font></font>", descricao);
-	fprintf(fp, "<img src=\"%s\" alt=\"%s\" style=\"width:354px;height:508px\"></center>", nomeImg, descricao);
+	fprintf(fp, "<center><font face=arial><font size=2><font color=#006600><h2>%s</h2></font></font></font>", descricao);
+	fprintf(fp, "<img class=resize src=\"%s\" alt=\"%s\" style=\"width:354px;height:508px\"></center>", nomeImg, descricao);
 	fputs("</body>",fp);
 }
 
